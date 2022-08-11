476e27860d61923c4dc105a1669e9f68084f5320
cryptichic <syberchief@gmail.com>
2022-08-10 20:15:16 -0500

    updated the deploy.js file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,26 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+
+const dotenv = require("dotenv");
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: process.env.REACT_APP_RINKEBY_URL,
+      accounts: [process.env.REACT_APP_PRRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.REACT_APP_ETHERSCAN_KEY,
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,22 +7,16 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const SimpleMint = await hre.ethers.getContractFactory("SimpleMint");
+  const simpleMint = await SimpleMint.deploy();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await simpleMint.deployed();
 
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  console.log("SimpleMint deployed to:", simpleMint.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;

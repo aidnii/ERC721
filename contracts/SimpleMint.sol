e13dd5643aafac72ae248f57e415e6d7db65b94e
cryptichic <syberchief@gmail.com>
2022-08-10 19:43:13 -0500

    added the varibles and constructor

---
+++ contracts/SimpleMint.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract SimpleMint is ERC721, Ownable {
+    uint256 public mintPrice = 0.05 ether;
+    uint256 public totalSupply;
+    uint256 public maxSupply;
+    bool public isMintEnabled;
+    mapping(address => uint256) public mintedWallets;
+
+    constructor() payable ERC721('Simple Mint', 'SM') {
+        maxSupply = 3;
+    }
+}
